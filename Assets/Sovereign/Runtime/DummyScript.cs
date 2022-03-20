5f693f3f2864e7e97c8286be769dbdc871c3b542
안팔아요 <42833717+Kicory@users.noreply.github.com>
2022-03-21 01:24:27 +0900

    패키지 테스트용 로그 기능
    아무것도 아님..

---
+++ Assets/Sovereign/Runtime/DummyScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace NoFS.DayLight.Sovereign {
+   public class DummyScript : MonoBehaviour {
+      public void doLog() {
+         Debug.Log("Log from Package");
+      }
+   } 
+}
